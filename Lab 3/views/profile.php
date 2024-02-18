b951af82a23ef4a2bbc8f85ec047b0e2b054fca9
Imtiaj Sajin <imtiajsajin@gmail.com>
2024-02-19 02:24:12 +0600

    Create profile.php

---
+++ Lab 3/views/profile.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Profile</title>
+    </head>
+    <body>
+        <h1>Profile</h1>
+        <table>
+            <tr>
+                <td>
+                    <fieldset style="height:300px;width:200px;">
+                        <legend>General Information</legend>
+                        <table>
+                            
+                            <tr>
+                                <td> <p></p> </td>
+                            </tr>
+            
+                            <tr>
+                                <td   ><b>First Name</b></td>
+                                <td> :</td>
+                                <?php
+                                    echo"<td> $_POST[fname] </td>";
+                                ?>
+                            </tr>
+            
+                            <tr>
+                                <td> <p></p> </td>
+                            </tr>
+            
+                            <tr>
+                                <td   ><b>Last Name</b></td>
+                                <td> :</td>
+                                <td>Kawsur</td>
+                            </tr>
+            
+                            <tr>
+                                <td> <p></p> </td>
+                            </tr>
+            
+                            <tr>
+                                <td   ><b>Father's Name</b></td>
+                                <td> :</td>
+                                <td>XYZ</td>
+                            </tr>
+            
+                            <tr>
+                                <td> <p></p> </td>
+                            </tr>
+            
+                            <tr>
+                                <td   ><b>Mother's Name</b></td>
+                                <td> :</td>
+                                <td>ZYX</td>
+                            </tr>
+            
+                            <tr>
+                                <td> <p></p> </td>
+                            </tr>
+            
+                            <tr>
+                                <td   ><b>Blood Group</b></td>
+                                <td> :</td>
+                                <td>B +</td>
+                            </tr>
+            
+                            <tr>
+                                <td> <p></p> </td>
+                            </tr>
+            
+                            <tr>
+                                <td   ><b>Religion</b></td>
+                                <td> :</td>
+                                <td>Islam</td>
+                            </tr>
+            
+                            <tr>
+                                <td> <p></p> </td>
+                            </tr>
+            
+                        </table>
+                    </fieldset>
+                </td>
+
+                <td>
+                    <fieldset style="height:200px;width:500px;">
+                        <legend>Contact Information</legend>
+                        <table>
+                            
+                            <tr>
+                                <td> <p></p> </td>
+                            </tr>
+            
+                            <tr>
+                                <td   ><b>Email</b></td>
+                                <td> :</td>
+                                <td>kawsur@aiub.edu</td>
+                            </tr>
+
+                            <tr>
+                                <td> <p></p> </td>
+                            </tr>
+            
+                            <tr>
+                                <td   ><b >Phone/Mobile</b></td>
+                                <td> :</td>
+                                <td>+880-01518XXXXXX</td>
+                            </tr>
+
+                            <tr>
+                                <td> <p></p> </td>
+                            </tr>
+            
+                            <tr>
+                                <td   ><b>Website</b></td>
+                                <td> :</td>
+                                <td>https://kawsur.me</td>
+                            </tr>
+
+                            <tr>
+                                <td> <p></p> </td>
+                            </tr>
+            
+                            <tr>
+                                <td   ><b>Present Address</b></td>
+                                <td> :</td>
+                                <td>408/1, Kuratoli, Khilkhet, Dhaka 1229,Bangladesh </td>
+                            </tr>
+                        
+                        </table>
+                    </fieldset>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
